8b28cfeb8ce9ae940623b1967331b20e118820d3
clpong <clpong@hotmail.com>
2015-09-11 11:20:26 +0800

    add files

---
+++ plot1.R
@@ -0,0 +1,14 @@
+setwd("C:/ExData_Plotting1")
+
+dataFile <- "./data/household_power_consumption.txt"
+data <- fread(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, na.strings="?", data.table=TRUE)
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower
+     , col="red"
+     , main="Global Active Power"
+     , xlab="Global Active Power (kilowatts)")
+dev.off()
